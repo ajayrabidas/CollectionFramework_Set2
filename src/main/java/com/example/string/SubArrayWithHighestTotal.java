57267d1406a5802e2a901599d37740c785add9da
ajayrabidas <ajayrabidas@gmail.com>
2025-03-27 16:06:28 +0530

    Updated with approach2

--- src/main/java/com/example/string/SubArrayWithHighestTotal.java
+++ src/main/java/com/example/string/SubArrayWithHighestTotal.java
@@ -14,6 +14,9 @@ public class SubArrayWithHighestTotal {
         int[] arr = {1, 8, 5, 3, 12, 10, 3, 2, 7};
         System.out.println("Maximum sum with sub-array size 5: " + t.method1(arr, 5));
 
+        // scenario 1 method 2
+        System.out.println("Maximum sum with sub-array size 5: " + t.method2(arr, 5));
+
     }
 
     // solution1:
@@ -33,8 +36,22 @@ public class SubArrayWithHighestTotal {
     }
 
     // solution2:
-    public void method2() {
+    public int method2(int[] arr, int subArrSize) {
 
+        int maxSum = 0;
+        // sum of first windows of size subArraySize
+        for (int i = 0; i < subArrSize; i++) {
+            maxSum = maxSum + arr[i];
+        }
+
+        // other remaining windows
+        int otherSum = maxSum;
+        for (int i = subArrSize; i < arr.length; i++) {
+            otherSum = otherSum + arr[i] - arr[i-subArrSize];
+            maxSum = Math.max(otherSum, maxSum);
+        }
+
+        return maxSum;
     }
 
 }
