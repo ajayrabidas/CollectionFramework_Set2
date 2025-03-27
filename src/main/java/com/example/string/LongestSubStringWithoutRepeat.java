7884343e3e4ae021309f024580ba8e7be22d2d15
ajayrabidas <ajayrabidas@gmail.com>
2025-03-27 13:09:55 +0530

    Updated code

---
+++ src/main/java/com/example/string/LongestSubStringWithoutRepeat.java
@@ -0,0 +1,41 @@
+package com.example.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubStringWithoutRepeat {
+
+// Question: Q8. Find length of longest substring without repeat
+
+    public static void main(String[] args) {
+        LongestSubStringWithoutRepeat t = new LongestSubStringWithoutRepeat();
+
+        // scenario 1 method 1
+        System.out.println("Max Lenght of sub-string without repetetion: " + t.method1("aabcdefghabce"));
+    }
+
+    // solution1:
+    public int method1(String str) {
+
+        int maxLen = Integer.MIN_VALUE;
+        Set<Character> se = new HashSet<Character>();
+        int l = 0;
+        int r;
+        for (r = 0; r < str.length(); r++) {
+            if (se.contains(str.charAt(r)) && l < r) {
+                se.remove(str.charAt(l));
+                l++;
+            }
+            se.add(str.charAt(r));
+            maxLen = Math.max(maxLen, (r - l + 1));
+        }
+
+        return maxLen;
+    }
+
+    // solution2:
+    public void method2() {
+
+    }
+
+}
