2a8e0961ea888231b194c76797d33d75c84df05a
ajayrabidas <ajayrabidas@gmail.com>
2026-03-25 17:18:51 +0530

    Check Palindrome

---
+++ src/main/java/com/example/string/PalindromeCheck.java
@@ -0,0 +1,36 @@
+package com.example.string;
+
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+
+        PalindromeCheck pc = new PalindromeCheck();
+        pc.checkPalindrome("kanak");
+        pc.checkPalindrome("ABCD");
+        pc.checkPalindrome("");
+
+    }
+
+    public boolean checkPalindrome(String str) {
+
+        if (str == null || str.isEmpty()) {
+            System.out.println("Provide a valid input");
+            return false;
+        }
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                System.out.println("Not a palindrome");
+                return false;
+            }
+            i++;
+            j--;
+        }
+        System.out.println("It is a palindrome");
+        return true;
+    }
+
+}
