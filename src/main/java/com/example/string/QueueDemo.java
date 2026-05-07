a194f8b47d44e45d45b675877db5ba1895f61240
ajayrabidas <ajayrabidas@gmail.com>
2026-05-07 23:55:19 +0530

    Demo showing operations on Queue using LinkedList

---
+++ src/main/java/com/example/string/QueueDemo.java
@@ -0,0 +1,25 @@
+package com.example.string;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+
+        // Adding elements
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+        queue.offer("D");
+
+        System.out.println("Queue is: " + queue);
+
+        System.out.println("Getting front element: " + queue.peek()); // queue.peek() gets "A"
+
+        System.out.println("Removing Element: " + queue.poll()); // queue.poll]() removes "A"
+
+        System.out.println("Queue after removing: " + queue); // shows queue after removal of "A"
+    }
+}
