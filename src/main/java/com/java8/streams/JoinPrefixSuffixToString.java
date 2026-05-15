b3b7d3749641ddadbbc25fa78c933944fe54b02d
ajayrabidas <ajayrabidas@gmail.com>
2026-05-15 17:24:56 +0530

    Joining list of strings with prefix and suffix

---
+++ src/main/java/com/java8/streams/JoinPrefixSuffixToString.java
@@ -0,0 +1,18 @@
+package com.java8.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Given a list of strings, join the strings with ‘[‘ as prefix, ‘]’ as suffix and ‘,’ as delimiter?
+public class JoinPrefixSuffixToString {
+
+    public static void main(String[] args) {
+
+        List<String> lis = Arrays.asList("Book", "Pen", "Penciil", "Notebook", "Scissors");
+        String joinedString = lis.stream().collect(Collectors.joining(", ", "[", "]"));
+
+        System.out.println(joinedString);
+    }
+
+}
