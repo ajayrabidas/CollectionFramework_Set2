3709aae8e0c6de6c0ff5f5d51ecc825f2b3c6668
ajayrabidas <ajayrabidas@gmail.com>
2026-06-07 22:54:29 +0530

    Sum and average of integer ArrayRemoveDuplicates

---
+++ src/main/java/com/java8/streams/SumAndAverageOfIntArray.java
@@ -0,0 +1,17 @@
+package com.java8.streams;
+
+import java.util.Arrays;
+
+// Given an integer array, find sum and average of all elements
+public class SumAndAverageOfIntArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{45, 12, 56, 15, 24, 75, 31, 89};
+
+        int sum = Arrays.stream(arr).sum();
+        System.out.println("Sum is: " + sum);
+
+        double avg = Arrays.stream(arr).average().getAsDouble();
+        System.out.println("Average is: " + avg);
+    }
+}
