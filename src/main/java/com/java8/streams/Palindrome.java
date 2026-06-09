8811a4d6376c33b2c4caf77c734a1d671239bde8
ajayrabidas <ajayrabidas@gmail.com>
2026-06-09 17:19:51 +0530

    Palindrome using java 8

---
+++ src/main/java/com/java8/streams/Palindrome.java
@@ -0,0 +1,20 @@
+package com.java8.streams;
+
+import java.util.stream.IntStream;
+
+// Palindrome program using Java 8 streams
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String str = "ROTATOR";
+
+        boolean isPalindrome = IntStream.range(0, str.length() / 2)
+                .noneMatch(i -> str.charAt(i) != str.charAt(str.length() - i - 1));
+
+        if (isPalindrome)
+            System.out.println("It is palindrome");
+
+        if (!isPalindrome)
+            System.out.println("It is NOT a palindrome");
+    }
+}
