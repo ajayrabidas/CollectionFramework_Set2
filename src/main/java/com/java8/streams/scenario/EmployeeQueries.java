7a5e23949bf975f5478519f0347681d9676552d5
ajayrabidas <ajayrabidas@gmail.com>
2026-06-23 15:09:48 +0530

    Get Average age of Males and Females

--- src/main/java/com/java8/streams/scenario/EmployeeQueries.java
+++ src/main/java/com/java8/streams/scenario/EmployeeQueries.java
@@ -9,9 +9,11 @@ public class EmployeeQueries {
     public static void main(String[] args) {
         List<Employee> employeeList = EmployeeData.addEmployeeData();
 
+
         System.out.println("How many male and female employees are there in the organization?");
         Map<String, Long> noOfMaleFemaleEmployees = employeeList.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
         System.out.println(noOfMaleFemaleEmployees);
+
 
         System.out.println("Print the name of all departments in the organization?");
         employeeList.stream().map(Employee::getDepartment)
@@ -19,5 +21,12 @@ public class EmployeeQueries {
                 .forEach(System.out::println);
 
 
+        System.out.println("What is the average age of male and female employees?");
+        Map<String, Double> avgAgeOfMaleAndFemale = employeeList.stream()
+                .collect(
+                        Collectors
+                                .groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
+
+        System.out.println("Average ages are: " + avgAgeOfMaleAndFemale);
     }
 }
