a2312fc5034888fcec1a1fe898511e1a61858c84
ajayrabidas <ajayrabidas@gmail.com>
2026-07-27 23:15:22 +0530

    Find Equilibrium Index for a given integer array

---
+++ src/main/java/com/hundred/problem/EquilibriumIndex.java
@@ -0,0 +1,25 @@
+package com.hundred.problem;
+/*
+Find equilibrium index
+An index is called an equilibrium index if:
+Sum of elements on the left = Sum of elements on the right
+The element at the equilibrium index itself is not included in either sum.
+        Example
+        Array = { -7, 1, 5, 2, -4, 3, 0 }
+        Index = 3
+
+    At index 3:
+    Left Sum  = -7 + 1 + 5 = -1
+    Right Sum = -4 + 3 + 0 = -1
+    Therefore:
+    Equilibrium Index = 3
+*/
+
+public class EquilibriumIndex {
+    public static void main(String[] args) {
+        int[] arr = {-7, 1, 5, 2, -4, 3, 0};
+
+
+
+    }
+}
