d0742cdc522e4700c0506b5fd2bd52c78e8352d2
ajayrabidas <ajayrabidas@gmail.com>
2026-08-05 23:54:31 +0530

    Finding majority element in an array

---
+++ src/main/java/com/hundred/problem/FindMajorityElement.java
@@ -0,0 +1,21 @@
+package com.hundred.problem;
+
+/*
+Find majority element
+Given an array of size N, find the majority element.
+A majority element is an element that appears more than N/2 times.
+If no majority element exists, return -1.
+
+Example 1
+Input  : [2, 2, 1, 1, 2, 2, 2]
+
+Output : 2
+*/
+
+public class FindMajorityElement {
+
+    public static void main(String[] args) {
+
+    }
+
+}
