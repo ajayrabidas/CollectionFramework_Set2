4bafa234c6adfc85aca0898b8a57c586abb28fe1
ajayrabidas <ajayrabidas@gmail.com>
2026-08-06 00:00:02 +0530

    Finding majority element in an array

--- src/main/java/com/hundred/problem/FindMajorityElement.java
+++ src/main/java/com/hundred/problem/FindMajorityElement.java
@@ -12,9 +12,28 @@ Input  : [2, 2, 1, 1, 2, 2, 2]
 Output : 2
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindMajorityElement {
 
     public static void main(String[] args) {
+
+        int[] arr = {2, 2, 1, 1, 2, 2, 2};
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        int limit = arr.length / 2;
+
+        for (Map.Entry<Integer, Integer> me : map.entrySet()) {
+            if (me.getValue() > limit) {
+                System.out.println(me.getKey());
+            }
+        }
 
     }
 
